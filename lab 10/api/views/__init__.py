c4a77fbe15bad6d618b2bc4fb69171232412b912
gulsezim12 <gulsezimbilimzhankyzy@gmail.com>
2026-04-08 17:01:06 +0500

    added file

---
+++ lab 10/api/views/__init__.py
@@ -0,0 +1,10 @@
+# from .fbv import products_list, product_detail
+# from .cbv import ProductListAPIView, ProductDetailAPIView
+# from .generic_v1 import ProductListAPIView, ProductDetailAPIView
+from .generic_v2 import (
+    ProductListAPIView,
+    ProductDetailAPIView,
+    CategoryListAPIView,
+    CategoryDetailAPIView,
+    CategoryProductsAPIView
+)
